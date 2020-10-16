2a0eb8b8a051d40de0a5b57a636feb5e2d4fed6b
Alex McKee <alexander.mckee@wsu.edu>
2020-10-16 14:40:30 -0700

    Second iteration, Includes Tets

--- src/test/java/app/gpx_animator/PhotosTest.java
+++ src/test/java/app/gpx_animator/PhotosTest.java
@@ -2,43 +2,41 @@ package app.gpx_animator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class PhotosTest {
 
-	
+
     @Test
     void createFadedInPhoto() {
-         
+
         Photos testee = new Photos(null);
         ArrayList<BufferedImage> increasingPhoto = new ArrayList<BufferedImage>();
-        
+
         int width = 100;
         int height = 100;
         int numIncreasingFrames = 40;
-        
+
         // private photos method readPhoto ensures the photo is scaled to fit in the image its being drawn on
         BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         BufferedImage photoImage = new BufferedImage(70, 70, BufferedImage.TYPE_INT_RGB);
 
-        
+
         final Graphics2D g2d = bi.createGraphics();
         final int posX = (bi.getWidth() - photoImage.getWidth()) / 2;
         final int posY = (bi.getHeight() - photoImage.getHeight()) / 2;
 
         increasingPhoto = testee.createFadedInPhoto(bi, photoImage, g2d, posX, posY, numIncreasingFrames);
-        
+
         // Assert we have created the correct number of frames
         assertEquals(numIncreasingFrames, increasingPhoto.size());
-        
+
         // Assert that two frames in the increasing photo series are different. That the photo has infact increased
         assertNotEquals(increasingPhoto.get(0), increasingPhoto.get(1));
 
